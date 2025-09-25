e15a7f6dbb7c20025ac46ec1db274eedc743cb1f
Helikop <gera-60498@mail.ru>
2025-09-25 17:48:18 +0300

    init

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -26,21 +26,27 @@ class CommonResource{
     int x=0;
 }
 // # 2
-class CountThread implements Runnable{
+class CountThread implements Runnable {
 
     CommonResource res;
-    CountThread(CommonResource res){
-        this.res=res;
+
+    CountThread(CommonResource res) {
+        this.res = res;
     }
-    public void run(){
-        res.x=1;
-        for (int i = 1; i < 5; i++){
-            System.out.printf("%s %d \n", Thread.currentThread().getName(), res.x);
-            res.x++;
-            try{
-                Thread.sleep(100);
+
+    public void run() {
+
+        synchronized (res) { // Этот оператор предваряет блок кода или метод, который подлежит синхронизации
+            // При применении оператора synchronized к методу пока этот метод не завершит выполнение, монопольный доступ имеет только один поток - первый, который начал его выполнение.
+            res.x = 1;
+            for (int i = 1; i < 5; i++) {
+                System.out.printf("%s %d \n", Thread.currentThread().getName(), res.x);
+                res.x++;
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                }
             }
-            catch(InterruptedException e){}
         }
     }
 }
@@ -48,28 +54,29 @@ class CountThread implements Runnable{
 public class Main {
     public static void main(String[] args) {
 
-        Thread t = Thread.currentThread(); // получаем главный поток
-        System.out.println(t.getName()); // main
-        System.out.println(t); // main
+//        Thread t = Thread.currentThread(); // получаем главный поток
+//        System.out.println(t.getName()); // main
+//        System.out.println(t); // main
+//
+//        // Создаём свой класс на основе Thread
+//        System.out.println("Main thread started...");
+//        new JThread("JThread").start();
+//        System.out.println("Main thread finished...\n\n");
+//
+//        // Запуск нескольких потоков
+//        // # 1
+//        System.out.println("Main thread started...");
+//        for(int i=1; i < 6; i++)
+//            new JThread("JThread " + i).start();
+//        System.out.println("Main thread finished...");
+//
+//        try {
+//            Thread.sleep(5000);
+//        }
+//        catch(InterruptedException e){
+//            System.out.println("Thread has been interrupted");
+//        }
 
-        // Создаём свой класс на основе Thread
-        System.out.println("Main thread started...");
-        new JThread("JThread").start();
-        System.out.println("Main thread finished...\n\n");
-
-        // Запуск нескольких потоков
-        // # 1
-        System.out.println("Main thread started...");
-        for(int i=1; i < 6; i++)
-            new JThread("JThread " + i).start();
-        System.out.println("Main thread finished...");
-
-        try {
-            Thread.sleep(5000);
-        }
-        catch(InterruptedException e){
-            System.out.println("Thread has been interrupted");
-        }
         // # 2
         CommonResource commonResource= new CommonResource();
         for (int i = 1; i < 6; i++) {
